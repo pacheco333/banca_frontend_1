b81b284bbf01f1d780ef33e1e0b29403f1f2e380
Pacheco333 <santiago2006ortizp@gmail.com>
2025-11-24 01:12:23 -0500

    enviroment prod

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, BehaviorSubject, tap } from 'rxjs';
 import { Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 
 // ========================================
 // INTERFACES

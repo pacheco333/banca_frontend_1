89cc45fc7ff32496a230cb0356bb86be8e8b2812
Pacheco333 <santiago2006ortizp@gmail.com>
2025-11-24 21:06:49 -0500

    url para consultar desde solicitar

--- src/app/features/asesor/services/solicitud.service.ts
+++ src/app/features/asesor/services/solicitud.service.ts
@@ -7,7 +7,7 @@ import { AuthService } from '../../../core/services/auth.service';
   providedIn: 'root'
 })
 export class SolicitudService {
-  private apiUrl = 'http://localhost:3000/api/asesor';
+  private apiUrl = 'https://banca-backend-1.onrender.com/api/asesor';
 
   constructor(
     private http: HttpClient,
